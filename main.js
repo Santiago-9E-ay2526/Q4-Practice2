da46110644e9e25488a95e0ff3df1a1007bf64b0
Santiago <santiago.mj@obmontessori.edu.ph>
2026-03-26 13:21:09 +0800

    update main

---
+++ main.js
@@ -0,0 +1,18 @@
+function gradeDescription() {
+     let schoolGrade = Number(document.getElementById('schoolGrade').value);
+          if (schoolGrade >= 94 && schoolGrade <= 100) {
+               window.alert ("Excellent");
+          }
+          else if (schoolGrade >= 87 && schoolGrade <= 93) {
+               window.alert ("Above Standard");
+          }
+          else if (schoolGrade >= 80 && schoolGrade <= 86) {
+               window.alert ("Standard");
+          }
+          else if (schoolGrade >= 75 && schoolGrade <= 79) {
+               window.alert ("Needs Improvement");
+          }
+          else {
+               window.alert ("Failed");
+          }
+}
